906fb600e62706f95a1640027e5d30be21acd323
Tania <nathania.santa21@students.unila.ac.id>
2024-10-15 12:33:49 +0700

    model instructor sql update

---
+++ models/dimentions/dim_instructor.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+
+with stg_instructor as (
+    select distinct Buddy as instructor_name
+    FROM staging_1.all_batch
+    where Buddy is not null
+    and Buddy != '#REF!'                -- Exclude "#REF!" values
+      and Buddy != '-'  
+)
+
+select row_number() over (order by `instructor_name`) as instructor_id, instructor_name
+from stg_instructor
+ 
---
+++ models/dimentions/dim_student.sql
@@ -0,0 +1,14 @@
+{{ config(materialized='table') }}
+
+
+with stg_student as (
+    select distinct `Name` as student_name
+    FROM staging_1.all_batch
+    where `Name` is not null
+    and `Name` != '#REF!'                -- Exclude "#REF!" values
+      and `Name` != '-'  
+)
+
+select row_number() over (order by `student_name`) as student_id, student_name
+from stg_student
+ 
